a6afc91833c99ac01ef21e90117bab7983dbb60e
Avery Merlo <merloav@bc.edu>
2023-01-12 00:44:48 -0500

    Initial Commit

---
+++ NBA/NBAApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBAApp.swift
+//  NBA
+//
+//  Created by Avery Merlo on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct NBAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
